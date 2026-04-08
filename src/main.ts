00e2c2ce10445c9c1711b2103cc2de3f7580fb92
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-04-08 12:13:50 +0000

    push 12H13

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log'],
-    bufferLogs: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-     origin: 'http://network-manager-ui.vercel.app',   // autorise uniquement votre frontend
+     origin: 'https://network-manager-ui.vercel.app',   // autorise uniquement votre frontend
      methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     credentials: true,                 // si vous utilisez cookies / authorization headers
     allowedHeaders: 'Content-Type, Accept, Authorization',
   })
 
-  app.use((req, res, next) => {
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('X-Frame-Options', 'DENY');
-    res.setHeader('X-XSS-Protection', '1; mode=block');
-    next();
-  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
