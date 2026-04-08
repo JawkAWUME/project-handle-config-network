278363f3525b3aaa863e30005036988258a03f0e
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-04-08 12:20:28 +0000

    Push 12H20

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-     origin: 'https://network-manager-ui.vercel.app',   // autorise uniquement votre frontend
-     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200'
+,   // autorise uniquement votre frontend
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,                 // si vous utilisez cookies / authorization headers
     allowedHeaders: 'Content-Type, Accept, Authorization',
   })
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
