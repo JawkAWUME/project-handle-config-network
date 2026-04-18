e377344a5188265b5de738137b542b3539f7b15e
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-04-18 04:04:04 +0000

    push 4H03

--- src/firewalls/firewalls.module.ts
+++ src/firewalls/firewalls.module.ts
@@ -6,11 +6,12 @@ import { Firewall } from './firewall.entity';
 import { ConfigurationHistory } from '../config-history/config-history.entity';
 import { PendingChangeService } from 'src/pending-change/pending-changes.service';
 import { PendingChange } from 'src/pending-change/pending-change.entity';
+import { RoutersService } from 'src/routers/routers.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Firewall, ConfigurationHistory,PendingChange])],
   controllers: [FirewallsController],
-  providers: [FirewallsService,PendingChangeService],
-  exports: [FirewallsService,PendingChangeService],
+  providers: [FirewallsService,PendingChangeService,RoutersService],
+  exports: [FirewallsService,PendingChangeService,RoutersService],
 })
 export class FirewallsModule {}
