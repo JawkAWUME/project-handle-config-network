db7c1a9fae9139e1c40bbdd52e306d0a52477ae1
moussabd <109081623+JawkAWUME@users.noreply.github.com>
2026-04-18 13:15:38 +0000

    push 13H15

--- src/routers/routers.module.ts
+++ src/routers/routers.module.ts
@@ -7,9 +7,12 @@ import { ConfigHistoryModule } from '../config-history/config-history.module';
 import { ConfigurationHistory } from '../config-history/config-history.entity';
 import { PendingChange } from 'src/pending-change/pending-change.entity';
 import { PendingChangeService } from 'src/pending-change/pending-changes.service';
+import { FirewallsModule } from 'src/firewalls/firewalls.module';
+import { SitesModule } from 'src/sites/sites.module';
+import { SwitchesModule } from 'src/switchs/switch.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Router, ConfigurationHistory,PendingChange])],
+  imports: [TypeOrmModule.forFeature([Router, ConfigurationHistory,PendingChange]), SwitchesModule,SitesModule],
   controllers: [RoutersController],
   providers: [RoutersService,PendingChangeService],
   exports: [RoutersService,PendingChangeService],
